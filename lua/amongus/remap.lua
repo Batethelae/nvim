a4e62b9c03d7429be2dece4b8e388d60f7a5a722
Max <maxwellmcnally@hotmail.com>
2024-03-04 15:51:03 -0500

    U to nop

--- lua/amongus/remap.lua
+++ lua/amongus/remap.lua
@@ -1,5 +1,7 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n","U","<nop>");
+
 --moving around highlighted lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
